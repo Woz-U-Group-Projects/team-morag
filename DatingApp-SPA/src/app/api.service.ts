e3c09b372ebe29c9a2efbb4dbc1548a3026aed11
Derek Dobrovolny <drdobrovolny@gmail.com>
2019-11-26 20:17:10 -0600

    news api work
    added news api for news component (needs styling)

---
+++ DatingApp-SPA/src/app/api.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+    API_KEY = '83361ab188d840e3b5b51f3d4863498b';
+    
+  constructor(private httpClient: HttpClient) { }
+  public getNews(){
+    return this.httpClient.get(`https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=${this.API_KEY}`);
+  }
+}
--- DatingApp-SPA/src/app/news/news.component.ts
+++ DatingApp-SPA/src/app/news/news.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ApiService } from '../api.service';
 @Component({
   selector: 'app-news',
   templateUrl: './news.component.html',
   styleUrls: ['./news.component.css']
 })
 export class NewsComponent implements OnInit {
-
-  constructor() { }
-
+  articles: any;
+  constructor(private apiService: ApiService) { }
   ngOnInit() {
+    this.apiService.getNews().subscribe((data)=>{
+      console.log(data);
+     
+      this.articles = data['articles'];
+    });
   }
-
 }
