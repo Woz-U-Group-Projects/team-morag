110907b2f69b8e8a69c39e1859fe6afa6a05ccb1
Derek Dobrovolny <drdobrovolny@gmail.com>
2019-11-27 22:22:49 -0600

    news item model
    adding model file for news-item

---
+++ DatingApp-SPA/src/app/_models/news-item.ts
@@ -0,0 +1,12 @@
+export interface NewsItem{
+    id: string;
+    name: string;
+    author: string;
+    title: string;
+    description: string;
+    url: string;
+    urlToImage:string;
+    publishedAt:string;
+    content:string;
+    
+}
